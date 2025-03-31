2eccf32d6d0ac0f40c8ca76f749749597c1f4510
RoninBuilder <23f2004770@ds.study.iitm.ac.in>
2025-03-31 22:06:59 +0530

    Delete reset.js

--- reset.js
+++
@@ -1,6 +0,0 @@
-inputs = document.querySelectorAll('input')
-textboxes = document.querySelectorAll("textarea")
-buttons  = document.querySelectorAll("button")
-inputs.forEach(input => input.removeAttribute('disabled'));
-buttons.forEach(input => input.removeAttribute('disabled'));
-textboxes.forEach(input => input.removeAttribute('disabled'));
